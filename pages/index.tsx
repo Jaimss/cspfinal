8425ed480a6f55526df37aa2c6e9a2a485185980
jtsokos <tsokos.jonathan@tampaprep.org>
2021-04-21 09:37:45 -0400

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,9 @@ export default function Home({ data }): JSX.Element {
 
   return (
     <div className="page-container">
+      <div className="logo-container">
+        <img src="./tplogo.png" alt="logo" className="logo" />
+      </div>
       <div id="theme-switcher" onClick={() => {
         // code to switch theme here
         const root = document.getElementsByTagName('html')[0]
@@ -23,58 +26,60 @@ export default function Home({ data }): JSX.Element {
         setJoke(dadjoke.random())
       }}>Switch Theme</div>
       {/*event name & date header*/}
-      <h3 className="name-header">Event Name</h3>
-      <h3 className="date-header">Event Date (24 HR)</h3>
-      {countdowns.map((countdown: CountdownProps) => {
-        return <Countdown key={countdown.name} name={countdown.name} year={countdown.year} month={countdown.month} day={countdown.day} hour={countdown.hour} minute={countdown.minute} onClick={() => {
-          countdowns.splice(countdowns.indexOf(countdown), 1)
-          setCountdowns([...countdowns])
+      <div className="date-container">
+        <h3 className="name-header">Event Name</h3>
+        <h3 className="date-header">Event Date (24 HR)</h3>
+        {countdowns.map((countdown: CountdownProps) => {
+          return <Countdown key={countdown.name} name={countdown.name} year={countdown.year} month={countdown.month} day={countdown.day} hour={countdown.hour} minute={countdown.minute} onClick={() => {
+            countdowns.splice(countdowns.indexOf(countdown), 1)
+            setCountdowns([...countdowns])
+            sendCountdownAPI([...countdowns])
+            setJoke(dadjoke.random())
+          }} />
+        })}
+
+        <input type="text" name="name" id="name" />
+        <input type="datetime-local" name="date" id="date" />
+        <input type="submit" value="Submit" id="submit" onClick={() => {
+          const name = (document.getElementById("name") as HTMLInputElement).value
+          const dateStr = (document.getElementById("date") as HTMLInputElement).value
+
+          if (name == "" || dateStr == "") {
+            alert("Please fill out the form fields completely!")
+            return
+          }
+
+          if (countdowns.some((item) => item.name === name)) {
+            alert("Please use a different Name!")
+            return
+          }
+
+          const date = new Date(dateStr)
+
+          var min;
+          if (date.getMinutes() == 0) {
+            min = "00"
+          } else if (date.getMinutes() >= 1 && date.getMinutes() <= 9) {
+            min = "0" + date.getMinutes().toString()
+          } else {
+            min = date.getMinutes()
+          }
+
+          const countdownTemp = {
+            name: name,
+            year: date.getFullYear(),
+            day: date.getDate(),
+            month: date.getMonth() + 1,
+            minute: min,
+            hour: date.getHours()
+          }
+          countdowns.push(countdownTemp)
           sendCountdownAPI([...countdowns])
+          setCountdowns([...countdowns])
           setJoke(dadjoke.random())
         }} />
-      })}
-
-      <input type="text" name="name" id="name" />
-      <input type="datetime-local" name="date" id="date" />
-      <input type="submit" value="Submit" id="submit" onClick={() => {
-        const name = (document.getElementById("name") as HTMLInputElement).value
-        const dateStr = (document.getElementById("date") as HTMLInputElement).value
-
-        if (name == "" || dateStr == "") {
-          alert("Please fill out the form fields completely!")
-          return
-        }
-
-        if (countdowns.some((item) => item.name === name)) {
-          alert("Please use a different Name!")
-          return
-        }
-
-        const date = new Date(dateStr)
-
-        var min;
-        if (date.getMinutes() == 0) {
-          min = "00"
-        } else if (date.getMinutes() >= 1 && date.getMinutes() <= 9) {
-          min = "0" + date.getMinutes().toString()
-        } else {
-          min = date.getMinutes()
-        }
-
-        const countdownTemp = {
-          name: name,
-          year: date.getFullYear(),
-          day: date.getDate(),
-          month: date.getMonth() + 1,
-          minute: min,
-          hour: date.getHours()
-        }
-        countdowns.push(countdownTemp)
-        sendCountdownAPI([...countdowns])
-        setCountdowns([...countdowns])
-        setJoke(dadjoke.random())
-      }} />
-      <p className="dadjoke">{joke}</p>
+        <p className="dadjoke">{joke}</p>
+      </div>
     </div>
   )
 }
