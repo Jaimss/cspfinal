0b2e897b4c9a9bcb19d1528092bd3a34c459b802
jtsokos <tsokos.jonathan@tampaprep.org>
2021-04-22 09:30:29 -0400

    fixed it all

--- pages/api/countdowns.tsx
+++ pages/api/countdowns.tsx
@@ -3,17 +3,18 @@ import { CountdownProps } from "../../types/CountdownProps"
 
 const fs = require('fs')
 
-export default (req: NextApiRequest, res: NextApiResponse<Array<CountdownProps>>) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method == 'POST') {
         const json = JSON.stringify(req.body)
-        fs.writeFile('./data.json', json, err => {
-            if (err) {
-            } else {
-            }
-        })
+        fs.writeFile('./data.json', json, () => { })
+        res.status(200).end()
+        return
     } else if (req.method == 'GET') {
-        fs.readFile('./data.json', 'utf8', (err, json) => {
+        const json = fs.readFileSync('./data.json', 'utf8')
         res.status(200).json(json)
-        })
+        res.end()
+        return
     }
+    res.status(501).send('Not Implemented')
+    res.end()
 }
